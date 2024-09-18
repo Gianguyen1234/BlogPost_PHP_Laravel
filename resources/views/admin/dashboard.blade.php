8d1dda4e5f0f2b9db12ea807aee2165513df1d66
Thao Nguyen <nguyenluthao252@gmail.com>
2024-09-18 11:13:52 +0700

    cmt

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
     <title>Modern Bootstrap 5 Sidebar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{url('css/form.css')}}"> <!-- Your existing CSS link -->
+    <link rel="stylesheet" href="{{url('css/form.css')}}"> 
     <style>
         #lineChart {
             width: 100%;
