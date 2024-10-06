bfd5fef5b14b4d90421d21532db3e410d12d547d
Thao Nguyen <nguyenluthao252@gmail.com>
2024-10-06 21:08:12 +0700

    cmt

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,15 +3,6 @@
 @section('title', 'About Us')
 
 @section('content')
-<div class="container mt-5">
-    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-        <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-            src="https://www.canva.com/design/DAGSl9BuqXE/mn2XOHg3xT4676bvHtkvmw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-        </iframe>
-    </div>
-
 
     <!-- About Us Section -->
     <div class="jumbotron">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,6 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="container mt-5">
-    <!-- Floating Category Menu Button -->
-    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-        <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-            src="https://www.canva.com/design/DAGSmCCtsTE/VfccU4To4PgJWr_FMJtXRA/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-        </iframe>
-    </div>
-
 
     <!-- Carousel Section for Featured Posts -->
     <div class="carousel-container mb-5">
